a3bd1732ce1354ea2f5d2b545fdf2f0a1f0d69fb
laidongping <laidongping2006@sina.com>
2018-04-03 14:55:06 +0800

    commit 2

--- export_word_new.py
+++ export_word_new.py
@@ -213,30 +213,172 @@ class Section(object):
         """
         table_type = table_item.table_type
         tables = {
-            1: doc.add_table(1, 4), 
+            1: doc.add_table(1, 4), # 舆论数据模块表格 
+            2: doc.add_talbe(1, 4), # 典型报道模块表格
+            3: doc.add_talbe(1, 6), # 网民舆论模块表格
+            4: doc.add_table(1, 3), #语种分析模块表格 
+            5: doc.add_table(1, 3) #港澳台舆论模块表格 
         }
 
         table_handlers= {
             1: self.__render_1_table,
             2: self.__render_2_table,
-            3: self.__render_3_table
+            3: self.__render_3_table,
+            4: self.__render_4_table,
+            5: self.__render_5_table
         }
         if tables.get(table_type) and table_handlers.get(table_type):
             table = table[table_type]
             table_handler = table_handlers[table_type]
-            table_handler(table, table_item)
+            table_handler(table, table_item, table_type)
         else:
             print "get table or table handler wrong, type_type is %s" % table_type
 
-    def __render_1_table(self, table, table_item):
-        pass
+    def __render_table_header(self, table, tb_header_texts=[]):
+        tb_header = table.rows[0]
+        table.autofit = True
+        header_index = 0
+        for header_cell in tb_header.cells:
+            text = tb_header_texts[header_index]
+            cell_p = header_cell.add_paragrph(text)
+            header_index += 1
 
-    def __render_2_table(self, table, table_item):
-        pass
+    def __render_1_table(self, table, table_item, table_type=None):
+        """table_item.datas数据结构:
+        {"data":{"新闻": {"number": "", "groupData":[{"no": "", "name": "", "count": ""}]}}}
+            
+        """
+        tb_header_texts = ["", "序号", "名称", "数量"]
+        self.__render_table_header(table, tb_header_texts)
+        datas = table_item.datas
+        if datas and datas.get('data'):
+            data = datas["data"]
+            for site_type, value in data.itertiems():
+                total = value.get("number", 0)
+                group_data = value.get("groupData", [])
+                if group_data:
+                    group_data_len = len(group_data) 
+                    row_count = 0
+                    while row_count <= group_data_len:
+                        row = table.add_row()
+                        row_cells = row.cells
+                        # 第二行第一列开始合并上一行第一列
+                        if row_count > 0:
+                            cell_0 = row_cells[0]
+                            cell_0_merge = cell_0.merge(table.cell(row_count-1, 0))
+                        if row_count < group_data_len:
+                            group_item = group_data[row_count]
+                            if group_item:
+                                row_cells[1].add_paragraph(group_item.get("no", ""))
+                                row_cells[2].add_paragraph(group_item.get("name", ""))
+                                row_cells[3].add_paragraph(group_item.get("count", ""))
+                        if row_count == group_data_len:
+                            row_cells[1].merge(row_cells[2]).add_paragraph(u"小计")
+                            row_cells[3].add_paragraph(str(total))
+                            cell_0_merge.add_paragraph(site_type)
+                        row_count += 1
 
-    def __render_3_table(self, table, table_item):
-        pass
+    def __render_2_table(self, table, table_item, table_type=None):
+        table.autofit = True
+        datas = table_item.datas
+        if datas: 
+            data = datas
+            count = 0
+            seq = 1
+            for item in data:
+                if item: 
+                    title = item.get("title", "")
+                    group_number = item["Children"].get("groupNumber", "") if "Children" in item else ""
+                    number = item["Children"].get("number", "") if "Children" in item else ""
+                    group_name= item["Children"].get("groupName", "") if "Children" in item else ""
+                    content = item.get("Content", "")
+                    if seq == 1:
+                        i = 0
+                        while i < 4:
+                            if i == 0:
+                                table.cell(count, 0).add_paragraph(u"序号")
+                                table.cell(count, 1).add_paragraph(seq)
+                                table.cell(count, 2).add_paragraph(u"标题")
+                                table.cell(count, 3).add_paragraph(title)
+                            else:
+                                row = table.add_row()
+                                count += 1
+                                if i == 1:
+                                    table.cell(count, 0).add_paragraph(u"参与媒体数量（约）")
+                                    table.cell(count, 1).add_paragraph(group_number)
+                                    table.cell(count, 2).add_paragraph(u"共发报道数量（约）")
+                                    table.cell(count, 3).add_paragraph(number)
+                                elif i == 2:
+                                    table.cell(count, 0).add_paragraph(u"主要参与媒体")
+                                    table.cell(count, 1).merge(table.cell(count, 2)).merge(table.cell(count, 3)).add_paragraph(group_name)
+                                elif i == 3:
+                                    table.cell(count, 0).add_paragraph(u"内容概要")
+                                    table.cell(count, 1).merge(table.cell(count, 2)).merge(table.cell(count, 3)).add_paragraph(content)
+                            i += 1
+                        
+                    else:
+                        i = 0
+                        while i < 4:
+                            row = table.add_row()
+                            count += 1
+                            if i == 0:
+                                table.cell(count, 0).add_paragraph(u"序号")
+                                table.cell(count, 1).add_paragraph(seq)
+                                table.cell(count, 2).add_paragraph(u"标题")
+                                table.cell(count, 3).add_paragraph(title)
+                            if i == 1:
+                                table.cell(count, 0).add_paragraph(u"参与媒体数量（约）")
+                                table.cell(count, 1).add_paragraph(group_number)
+                                table.cell(count, 2).add_paragraph(u"共发报道数量（约）")
+                                table.cell(count, 3).add_paragraph(number)
+                            elif i == 2:
+                                table.cell(count, 0).add_paragraph(u"主要参与媒体")
+                                table.cell(count, 1).merge(table.cell(count, 2)).merge(table.cell(count, 3)).add_paragraph(group_name)
+                            elif i == 3:
+                                table.cell(count, 0).add_paragraph(u"内容概要")
+                                table.cell(count, 1).merge(table.cell(count, 2)).merge(table.cell(count, 3)).add_paragraph(content)
+                            i += 1
+                    seq += 1
+
+    def __render_3_table(self, table, table_item, table_type=None):
+        tb_header_texts = [u"序号", u"名称", u"微博数量", u"粉丝数量", u"关注数量", u"简介"]
+        self.__render_table_header(table, tb_header_texts)
+        self.__render_table_body(table, table_item, table_type)
         
+    def __render_4_table(self, table, table_item, table_type=None):
+        tb_header_texts = [u"序号", u"名称", u"数量"]
+        self.__render_table_header(table, tb_header_texts)
+        self.__render_table_body(table, table_item, table_type)
+
+    def __render_5_table(self, table, table_item, table_type=None):
+        tb_header_texts = [u"序号", u"名称", u"数量"]
+        self.__render_table_header(table, tb_header_texts)
+        self.__render_table_body(table, table_item, table_type)
+
+    def __render_table_body(self, table, table_item, table_type):
+        datas = table_item.datas
+        data = []
+        if table_type in [3, 4, 5]:
+            data = datas
+        if data:
+            count = 0
+            seq = 1
+            for item in data:
+                row = table.add_row()
+                row_cells = row.cells
+                if table_type == 3:
+                    row_cells[0].add_paragraph(seq)
+                    row_cells[1].add_paragraph(item.get("Author", ""))
+                    row_cells[2].add_paragraph(item.get("Posts", ""))
+                    row_cells[1].add_paragraph(item.get("Fans", ""))
+                    row_cells[1].add_paragraph(item.get("Follows", ""))
+                    row_cells[1].add_paragraph(item.get("Description", ""))
+                elif table_type in [4,]:
+                    row_cells[0].add_paragraph(seq)
+                    row_cells[1].add_paragraph(item.get("groupName", ""))
+                    row_cells[2].add_paragraph(item.get("count", ""))
+                seq += 1
+
     def _get_file_stream(self, file_identity):
         file_manager = self.file_manager
         file_stream = file_manager.get(file_identity)
